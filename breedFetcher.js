4adff4377c254b61016de891624daf9d559d3af6
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-12 14:52:14 -0500

    parse incoming json data and print to terminal

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
 request("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
-  console.log("error:", error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log("body:", body);
-  console.log(typeof body);
+  if (error) {
+    console.log("error:", error);
+    console.log('statusCode:', response && response.statusCode);
+  }
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
 });
