8e10ed6b0fd5f20e1fa22db2e1302d8c550cc3cb
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-12 15:20:31 -0500

    refactor ahead of testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,10 @@
 const request = require('request');
-const userRequest = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userRequest}`, (error, response, body) => {
-  if (error) {
-    console.log("error:", error.message);
-    console.log('statusCode:', response && response.statusCode);
-    return;
-  }
-  const data = JSON.parse(body);
-  try {
-    console.log(data[0].description);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    return callback(error, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
