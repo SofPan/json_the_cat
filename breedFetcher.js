1e56112830706dff028dc4e2a92dac12dec277a6
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-12 14:58:33 -0500

    accept user input as search parameter

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
+const userRequest = process.argv.slice(2);
 
-request("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${userRequest}`, (error, response, body) => {
   if (error) {
     console.log("error:", error);
     console.log('statusCode:', response && response.statusCode);
